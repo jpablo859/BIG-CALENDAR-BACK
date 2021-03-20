db3c144871987532dbcc434f382a9592a52e9f62
Juan Pablo <jpablo859@hotmail.com>
2021-03-20 18:39:41 -0500

    change

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const cors = require('cors');
 const {dbConnection} = require('./database/config');
 const app = express();
 
+const path = require('path');
+
 //database
 dbConnection();
 
@@ -15,11 +17,17 @@ app.use(cors());
 //directorio público
 app.use('/login', express.static('public') );
 
+
+
 //routes
 app.use(express.json());
 
 app.use('/api/auth', require('./routes/auth.routes'));
 app.use('/api/events', require('./routes/events.routes'));
+
+app.get('*', ( req, res ) => {
+    res.sendFile( path.join( __dirname+'/public/index.html' ) );
+});
 
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en el puerto ${PORT}`)
